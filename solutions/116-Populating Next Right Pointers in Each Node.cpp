67c8ba329fa3670b2e50d5f6d712e4384a38827f
lszero <lszero@users.noreply.github.com>
2015-06-27 09:54:10 +0800

    Create 116-Populating Next Right Pointers in Each Node.cpp

---
+++ solutions/116-Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+ 
+/* -------------------- Solution 1: BFS -------------------- */
+/*class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if(root == NULL) return;
+
+        queue<TreeLinkNode *> que;
+        que.push(root);
+        TreeLinkNode *now, *pre = NULL;
+        int i = 0, k = 1;
+        while(!que.empty()){
+            now = que.front();
+            que.pop();
+
+            if(pre) pre->next = now;
+
+            i++;
+            if(i == k){
+                now->next = NULL;
+                i = 0;
+                k *= 2;
+                pre = NULL;
+            }else{
+                pre = now;
+            }
+
+            if(now->left) que.push(now->left);
+            if(now->right) que.push(now->right);
+        }
+    }
+};*/
+
+/* -------------------- Solution 2: next pointer -------------------- */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        TreeLinkNode *p = root, *q;
+        while(p){
+            q = p;
+            while(q){
+                if(q->left){
+                    q->left->next = q->right;
+                    if(q->right){
+                        if(q->next){
+                            q->right->next = q->next->left;
+                        }else{
+                            q->right->next = NULL;
+                        }
+                    }
+                }
+                q = q->next;
+            }
+            p = p->left;
+        }
+    }
+};
