2a619fc04782d94bfb84430d218861afab2df1d5
lszero <lszero@users.noreply.github.com>
2015-08-28 18:03:41 +0800

    Update 264-Ugly Number II.cpp

--- solutions/264-Ugly Number II.cpp
+++ solutions/264-Ugly Number II.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-	int min(int a, int b, int c, int &p){
-    	p = 0;
-    	if(a > b) a = b, p = 1;
-    	if(a > c) a = c, p = 2;
-    	return a;
-	}
+    int min(int a, int b, int c, int &p){
+        p = 0;
+        if(a > b) a = b, p = 1;
+        if(a > c) a = c, p = 2;
+        return a;
+    }
 
     int nthUglyNumber(int n) {
     	int *ugly = new int[n+1];
@@ -13,11 +13,11 @@ public:
     	int pos[3];
     	pos[0] = 1, pos[1] = 1, pos[2] = 1;
         for(int i = 2; i <= n; i++){
-        	int p;
-        	int m = min(ugly[pos[0]] * 2, ugly[pos[1]] * 3, ugly[pos[2]] * 5, p); 
-        	pos[p]++;
-        	if(m != ugly[i-1]) ugly[i] = m;
-        	else i--;
+            int p;
+            int m = min(ugly[pos[0]] * 2, ugly[pos[1]] * 3, ugly[pos[2]] * 5, p); 
+            pos[p]++;
+            if(m != ugly[i-1]) ugly[i] = m;
+            else i--;
         }
         return ugly[n];
     }
