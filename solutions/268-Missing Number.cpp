7258219ee6abf844f10bf2d33ea02cc6dd37f086
lszero <lszero@users.noreply.github.com>
2015-08-28 09:26:09 +0800

    Create 268-Missing Number.cpp

---
+++ solutions/268-Missing Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+        	  sum += nums[i];
+        }
+        int n = nums.size();
+        return ((1+n)*n>>1) - sum;
+    }
+};
