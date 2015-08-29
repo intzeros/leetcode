99290ff570488918fe74417cd38353d776f6229e
lszero <lszero@users.noreply.github.com>
2015-08-29 19:06:50 +0800

    Update 258-Add Digits.cpp

--- solutions/258-Add Digits.cpp
+++ solutions/258-Add Digits.cpp
@@ -1,28 +1,28 @@
 /* ----------------- Solution 1: Recursion ----------------- */
 /*class Solution {
 public:
-  int getSum(int num){
+    int getSum(int num){
     int sum = 0;
-		while(num){
-			sum += num%10;
-			num /= 10;
-		}
-		return sum;
-	}
+        while(num){
+            sum += num%10;
+            num /= 10;
+        }
+        return sum;
+    }
 
     int addDigits(int num) {
-      if(num < 10) return num;
-      return addDigits(getSum(num));
+        if(num < 10) return num;
+        return addDigits(getSum(num));
     }
 };*/
 
 /* ----------------- Solution 2: o(1) ----------------- */
 class Solution {
 public:
-  int addDigits(int num) {
-    if(num == 0) return 0;
-    int rst = num % 9;
-    if(rst == 0) return 9;
-    else return rst;
-  }
+    int addDigits(int num) {
+        if(num == 0) return 0;
+        int rst = num % 9;
+        if(rst == 0) return 9;
+        else return rst;
+    }
 };
